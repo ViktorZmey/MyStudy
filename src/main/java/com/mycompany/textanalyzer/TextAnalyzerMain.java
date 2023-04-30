4e6e209091b8c2c0eac3420021f9d46cb2e95843
Viktor <victor.dyakonoff@gmail.com>
2023-04-30 13:17:52 +0700

    переименовал пакет textanalyzer

--- src/main/java/com/mycompany/textanalyzer/TextAnalyzerMain.java
+++ src/main/java/com/mycompany/textanalyzer/TextAnalyzerMain.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.Main.textanalyzermain;
+package com.mycompany.textanalyzer;
 
 import java.util.Arrays;
 
