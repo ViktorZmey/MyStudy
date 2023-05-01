adef2d7a99cb0266c70b2858d8d00a3bb451dbb6
Viktor <victor.dyakonoff@gmail.com>
2023-05-01 23:05:43 +0700

    набросок решение для задачи про Animal

---
+++ src/main/java/com/mycompany/classanimal/Cat.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classanimal;
+
+/**
+ *
+ * @author viktor
+ */
+public abstract class Cat extends Animal {
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public abstract void getDescription();
+
+    @Override
+    public abstract void eat();
+
+    @Override
+    public abstract void makeNoise();
+
+}
---
+++ src/main/java/com/mycompany/classanimal/Vet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classanimal;
+
+/**
+ *
+ * @author viktor
+ */
+public class Vet {
+    
+    void treatAnimal(Animal animal){
+        
+    }
+}
