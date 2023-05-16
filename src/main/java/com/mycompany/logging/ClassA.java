adcb24117d0482b2e2bf1fbb79b24c048d6f9ead
Viktor <victor.dyakonoff@gmail.com>
2023-05-16 23:59:56 +0700

    решение задачи Logging

---
+++ src/main/java/com/mycompany/logging/ClassA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.logging;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author viktor
+ */
+public class ClassA {
+    private static final Logger LOGGER = Logger.getLogger(ClassA.class.getName());
+    
+}
