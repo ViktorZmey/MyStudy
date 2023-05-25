d31d7cb3395739dc57736cfd05df69eb51accb86
Viktor <victor.dyakonoff@gmail.com>
2023-05-25 15:42:32 +0700

    3 вида реализации

--- src/main/java/com/mycompany/symmetricdifference/SymmetricDifferenceTest.java
+++ src/main/java/com/mycompany/symmetricdifference/SymmetricDifferenceTest.java
@@ -12,36 +12,71 @@ import java.util.*;
  */
 public class SymmetricDifferenceTest {
     public static void run() {
-        test();        
+//        test();
+        testPerformance();
     }
     
     static void test() {
-        Set<String> set1 = new LinkedHashSet<>();
-        set1.add("1");
-        set1.add("2");
-        set1.add("3");
-                
-        Set<String> set2 = new LinkedHashSet<>();
-        set2.add("0");
-        set2.add("1");
-        set2.add("2");
+        String[] strs1 = {"1", "2", "3"};
+        Set<String> set1 = new HashSet<>(Arrays.asList(strs1));
+        
+        String[] strs2 = {"0", "1", "2"};
+        Set<String> set2 = new HashSet<>(Arrays.asList(strs2));
                 
         System.out.println(symmetricDifference(set1, set2));
     }
     
-    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        Set<T> set = new HashSet<>();
-        set.addAll(set1);
-        set.addAll(set2);
+    static void testPerformance() {
+        int count = 100000;
+        var randomIntStreamIterator = (new Random()).ints(0, 10 * count).iterator();
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for (int i = 0; i < count; i++) {
+            set1.add(randomIntStreamIterator.next());
+            set2.add(randomIntStreamIterator.next());
+        }
         
-        Iterator<T> value = set.iterator();
-        while (value.hasNext()) {            
-            T values = value.next();
-            if (set1.contains(values) && set2.contains(values)) {
-                value.remove();                
-            }            
-        }                      
-        return set;
+        long start = System.nanoTime();
+        symmetricDifference(set1, set2);
+        long finish = System.nanoTime();
+        long timeElapsed = finish - start;
+        System.out.println(timeElapsed / 1000);
+    }
+    
+    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
+        if (set1.size() > set2.size()) {
+            var tmp = set1;
+            set1 = set2;
+            set2 = tmp;
+        }
+        var result = new HashSet<T>(set2);
+        set1.iterator().forEachRemaining((value) -> {
+            if (result.contains(value)) {
+                result.remove(value);
+            } else {
+                result.add(value);
+            }
+        });
+        return result;
+        
+//        var _set1 = new HashSet<T>(set1);
+//        var _set2 = new HashSet<T>(set2);
+//        _set1.removeAll(set2);
+//        _set2.removeAll(set1);
+//        _set1.addAll(_set2);
+//        return _set1;
+        
+//        Set<T> set = new HashSet<>();
+//        set.addAll(set1);
+//        set.addAll(set2);
+//        Iterator<T> iterator = set.iterator();
+//        while (iterator.hasNext()) {
+//            T value = iterator.next();
+//            if (set1.contains(value) && set2.contains(value)) {
+//                iterator.remove();        
+//            }            
+//        }                      
+//        return set;
     }
 }
 
