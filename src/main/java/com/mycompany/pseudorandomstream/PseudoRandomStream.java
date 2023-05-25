3b519b09dfcf8e052dad1435411b3dd11b3f949e
Viktor <victor.dyakonoff@gmail.com>
2023-05-25 20:38:37 +0700

    123

---
+++ src/main/java/com/mycompany/pseudorandomstream/PseudoRandomStream.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pseudorandomstream;
+
+import java.util.stream.IntStream;
+
+/**
+ *
+ * @author viktor
+ */
+public class PseudoRandomStream {
+    public static void run() {
+        test();
+    }
+    
+    static void test() {
+        System.out.println(pseudoRandomStream(13));
+    }
+    
+    public static IntStream pseudoRandomStream(int seed) {
+    return null; 
+    }
+}
